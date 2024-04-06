a9fbfdf176b68c5fdca539c29def175ad6b9cd91
Nishit1710 <nishitpatel10481@gmail.com>
2024-04-05 23:23:33 -0600

    Todo list and form rendered

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { View, Button } from 'react-native';
-
+import { Button, SafeAreaView, View , StyleSheet} from 'react-native';
+import ToDoList from '../components/ToDoList';
+import ToDoForm from "../components/ToDoForm";
 import MainLayout from '../layouts/MainLayout';
 
 const HomeScreen = ({ navigation }) => {
+  const addTask = (taskText) => {
+    setTasks([...tasks, taskText]);
+  };
+  const [tasks , setTasks] = React.useState([
+    'Wake Up at 8am', 
+    'Attend Class',
+    'Complete The Assignment',
+    'Go to Gym',
+ ]);
   return (
     <MainLayout>
-      <View>
-        <Button
-          title="Go to About"
-          onPress={() => navigation.navigate('About')}
-        />
-      </View>
+      <SafeAreaView>
+        <ToDoList tasks={tasks} />
+        <ToDoForm  addTask={addTask} />
+        <View style = {styles.button}>
+          <Button
+            title="Go to About"
+            onPress={() => navigation.navigate('About')}
+          />
+        </View>
+      </SafeAreaView>
     </MainLayout>
   );
 };
 
 export default HomeScreen;
+const styles = StyleSheet.create({
+  button: {
+    margin: 20,
+  },
+});
